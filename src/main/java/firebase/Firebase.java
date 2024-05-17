fef731b9c9eb5e8cb31837b2a12f04302a17ce30
Florin <xxnuafaxx@gmail.com>
2024-05-17 17:25:50 +0300

    Added data retrieval from database

--- src/main/java/firebase/Firebase.java
+++ src/main/java/firebase/Firebase.java
@@ -5,9 +5,7 @@ import benchmark.ComputerIdentifier;
 import benchmark.SystemSpecs;
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.firestore.*;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
@@ -15,14 +13,17 @@ import com.google.firebase.cloud.FirestoreClient;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class Firebase {
     public static void main(String[] args) throws Exception {
-        writeBenchmarkResult(new BenchmarkInfo("CPU",100,1));
+        SystemSpecs.isFirstRun();
+        writeBenchmarkResult(new BenchmarkInfo("CPU",100,1),new ComputerIdentifier());
+        System.out.println(getMyData("CPU",new ComputerIdentifier()));
     }
-    public static void writeBenchmarkResult(BenchmarkInfo benchmarkInfo) throws Exception {
+    public static void writeBenchmarkResult(BenchmarkInfo benchmarkInfo, ComputerIdentifier computerIdentifier) throws Exception {
         FileInputStream serviceAccount = new FileInputStream("resources/firebase/serviceAccountKey.json");
 
         FirebaseOptions options = FirebaseOptions.builder()
@@ -30,16 +31,13 @@ public class Firebase {
                 .build();
         FirebaseApp.initializeApp(options);
 
-        SystemSpecs.isFirstRun();
-
-        ComputerIdentifier computerIdentifier = new ComputerIdentifier();
-
         writeData(benchmarkInfo,computerIdentifier);
     }
 
     private static void writeData(BenchmarkInfo benchmarkInfo, ComputerIdentifier computerIdentifier) throws ExecutionException, InterruptedException, IOException {
         Firestore db = FirestoreClient.getFirestore();
         DocumentReference docRef = db.collection("users").document(computerIdentifier.getUUID());
+
         Map<String, Object> data = new HashMap<>();
         data.put("OS", computerIdentifier.getOs());
         data.put("CPU", computerIdentifier.getCpu());
@@ -48,13 +46,63 @@ public class Firebase {
         docRef.set(data);
 
         docRef = db.collection("users").document(computerIdentifier.getUUID()).collection("benchmarks").document(benchmarkInfo.getBenchmarkName());
+
         data = new HashMap<>();
-        //data.put("Benchmark Name", benchmarkInfo.getBenchmarkName());
         data.put("Score", benchmarkInfo.getScore());
         data.put("Time", benchmarkInfo.getTime());
         ApiFuture<WriteResult> result = docRef.set(data);
-        // ...
-        // result.get() blocks on response
-        System.out.println("Update time : " + result.get().getUpdateTime());
+
+        // result.get() blocks execution until response
+        result.get();
+    }
+    public static Double getMyData(String benchmarkName, ComputerIdentifier computerIdentifier){
+
+        Map<String, Object> data = null;
+        try {
+            Firestore db = FirestoreClient.getFirestore();
+            DocumentReference docRef = db.collection("users").document(computerIdentifier.getUUID()).collection("benchmarks").document(benchmarkName);
+
+            ApiFuture<DocumentSnapshot> future = docRef.get();
+            DocumentSnapshot document = future.get();
+            data = document.getData();
+
+        } catch (Exception e) {
+            return null;
+        }
+        return (Double) data.get("Score");
+    }
+
+    public static void getAllData(){
+        try {
+            List<QueryDocumentSnapshot> documents = getQueryDocumentSnapshots();
+
+            // Iterate through all the documents
+            for (QueryDocumentSnapshot document : documents) {
+                System.out.println("Document ID: " + document.getId());
+                System.out.println("Document Data: " + document.getData());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<QueryDocumentSnapshot> getQueryDocumentSnapshots() throws InterruptedException, ExecutionException {
+        Firestore db = FirestoreClient.getFirestore();
+
+        CollectionReference collectionRef = db.collection("your-collection");
+
+        // Asynchronously retrieve all documents
+        ApiFuture<QuerySnapshot> query = collectionRef.get();
+
+        // QuerySnapshot contains the results of the query
+        QuerySnapshot querySnapshot = query.get();
+
+        // Get a list of all documents in the collection
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        return documents;
     }
 }
+
+
+
+
